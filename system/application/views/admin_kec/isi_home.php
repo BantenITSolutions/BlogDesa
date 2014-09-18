cefa8a8bb3b12d4f0d425b08bebf8256dbf1aa45
Gede Lumbung <gedesumawijaya@gmail.com>
2014-09-18 10:55:36 +0800

    initial

---
+++ system/application/views/admin_kec/isi_home.php
@@ -0,0 +1,8 @@
+<div id="isi">
+<h1>Beranda</h1>
+Selamat datang <strong><?php echo $nama_user; ?></strong>, anda login dengan username <strong><?php echo $username; ?></strong>.<br>
+Sebagai admin Kecamatan <strong><?php echo $wilayah; ?></strong>.
+</div>
+</div>
+</body>
+</html>
